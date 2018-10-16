84d19900eb546ac88a6a248709fb146160dea8e5
Kahsius <cassius81.geek@free.fr>
2018-10-16 11:26:56 +0200

    MULTIPROCESSING git status!

--- main.py
+++ main.py
@@ -1,35 +1,37 @@
 #!/usr/bin/python3
 
 from pdb import set_trace
+from multiprocessing import Pool
 import sys
 import settings
 from classes.Game import Game
 import classes.analyzer as anzr
 
-# Import options
-settings.init()
-
-N_TEST = 10000
-if(len(sys.argv)) > 1:
-    N_TEST = int(sys.argv[1])
-
-results = []
-
-# Run games
-print("Test running...")
-for i in range(N_TEST):
+def get_results(i):
     g = Game()
-    results.append(g.run().values)
-print("... done")
+    results = g.run().values
+    return(results)
 
-# Start analysis
-print("Win Rate par round")
-wr = anzr.win_rates(results)
-for i in range(len(wr['names'])):
-    print(wr['names'][i] + " - " + str(wr['rates'][i]))
+if __name__ == '__main__':
+    settings.init()
 
-print("-"*30)
-print("Win Rate par partie")
-wr = anzr.win_rates_global(results)
-for i in range(len(wr['names'])):
-    print(wr['names'][i] + " - " + str(wr['rates'][i]))
+    N_TEST = 10000
+    if(len(sys.argv)) > 1:
+        N_TEST = int(sys.argv[1])
+
+    print("Test running...")
+    pool = Pool()
+    results = pool.map(get_results, range(N_TEST))
+    print("... done")
+
+    # Start analysis
+    print("Win Rate par round")
+    wr = anzr.win_rates(results)
+    for i in range(len(wr['names'])):
+        print(wr['names'][i] + " - " + str(wr['rates'][i]))
+
+    print("-"*30)
+    print("Win Rate par partie")
+    wr = anzr.win_rates_global(results)
+    for i in range(len(wr['names'])):
+        print(wr['names'][i] + " - " + str(wr['rates'][i]))
